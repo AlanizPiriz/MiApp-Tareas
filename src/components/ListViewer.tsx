6319e03b7fa96d560aff847d102be2b89acfa197
AlanizPiriz <alanizpiriz@gmail.com>
2025-08-19 20:59:39 -0300

    Primer Commit

--- src/components/ListViewer.tsx
+++ src/components/ListViewer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useAuth } from './AuthContext';
 import { subscribeToUserLists } from '../Services/firestoreHelpers';
 
--- src/components/TaskCreator.tsx
+++ src/components/TaskCreator.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { addTask } from '../Services/firestoreHelpers';
 import { useAuth } from './AuthContext';
 
