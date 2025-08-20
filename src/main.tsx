4db32edf7766a1d45e16c9a16e2082e0b928e5df
AlanizPiriz <alanizpiriz@gmail.com>
2025-08-19 21:14:57 -0300

    Primer Commit

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App';
 import './styles.css'
+import { AuthProvider } from './components/AuthContext';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <AuthProvider>
     <App></App>
+    </AuthProvider>
   </StrictMode>,
 )
    
