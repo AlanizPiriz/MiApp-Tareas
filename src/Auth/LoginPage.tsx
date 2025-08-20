023333327c2fe9abfac7723594b7c93a570b7816
Alan <alanizpiriz@gmail.com>
2025-08-20 00:37:31 -0300

    segundo commit

--- src/Auth/LoginPage.tsx
+++ src/Auth/LoginPage.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import { auth } from '../firebase';
 import { signInWithEmailAndPassword } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 
 export default function LoginPage() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
+  const navigate = useNavigate();
 
   const handleLogin = async () => {
     setError('');
@@ -38,6 +40,9 @@ export default function LoginPage() {
         Entrar
       </button>
       {error && <p style={{ color: 'red' }}>{error}</p>}
+      <button type='button' onClick={() => navigate('./registro')}>
+        Registrarse
+      </button>
     </div>
   );
 }
